1f4e63d211d8c4d414f6f42c0d9f8cda5ed4cfb3
Roy <rxlisbest@gmail.com>
2018-12-24 20:05:28 +0800

    mysql

---
+++ config/db.lua
@@ -0,0 +1,10 @@
+return {
+    host = "127.0.0.1",
+    port = 3306,
+    database = "lua_log_sql",
+    user = "root",
+    password = "root",
+    charset = "utf8",
+    -- prefix = "rrs_",
+    max_packet_size = 1024 * 1024,
+}
--- index.lua
+++ index.lua
@@ -1,1 +1,10 @@
 package.path = ngx.var.lua_dir .. "?.lua;;"
+local mysql = require "lib.resty.mysql"
+local db, err = mysql:new()
+if not db then
+    return
+end
+local ok, err, errcode, sqlstate = db:connect(mysql_config)
+if not ok then
+    return
+end
