b57a8e23ac2632a76dcdf7526e6a7479f233e9b6
Roy <rxlisbest@gmail.com>
2018-12-24 20:41:02 +0800

    db

--- index.lua
+++ index.lua
@@ -1,10 +1,13 @@
-package.path = ngx.var.lua_dir .. "?.lua;;"
+package.path = ngx.var.lua_log_sql_dir .. "?.lua;;"
 local mysql = require "lib.resty.mysql"
+local db_config = require "config.db"
+
 local db, err = mysql:new()
 if not db then
     return
 end
-local ok, err, errcode, sqlstate = db:connect(mysql_config)
+local ok, err, errcode, sqlstate = db:connect(db_config)
 if not ok then
+    ngx.say(err)
     return
 end
